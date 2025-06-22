294f10c64e3e2c7e696532d4c817b8d720a163b7
aubcsfw <aubcsfw@gmail.com>
2025-06-23 01:23:39 +1000

    Update index.js
    Taller

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     
         </p>
     
-    <iframe src="https://designer.spreadsheetweb.com/a/aus-shares-vs-property-v6" width="460" height="1400"></iframe>
+    <iframe src="https://designer.spreadsheetweb.com/a/aus-shares-vs-property-v6" width="460" height="1900"></iframe>
     
       </main>
 
