770ca77fe657a795091d9b030929e11c63506298
aubcsfw <aubcsfw@gmail.com>
2025-06-23 15:54:42 +1000

    Update index.js
    Remove header

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,6 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Australian shares versus property!" />
-        <p className="description">  </p>
-    
     <iframe src="https://designer.spreadsheetweb.com/a/aus-shares-vs-property-v6" width="460" height="700"></iframe>
     
       </main>
