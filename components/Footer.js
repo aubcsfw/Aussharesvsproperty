3c9722363b556613c486076b4e32c29f017e82b1
aubcsfw <aubcsfw@gmail.com>
2025-06-29 20:36:25 +1000

    Update Footer.js
    Add back to index link

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,8 @@ export default function Footer() {
     <>
 
     <a href="https://aubcsfw.github.io/clone1.html">Not working? Try clone 1. Each clone gets 300 uses/ mo</a>
-    <a href="https://github.com/aubcsfw/aussharesvsproperty/pages/clone2.html">Not working? Try clone 2. Each clone gets 300 uses/ mo</a>
+    <a href="https://github.com/aubcsfw/aussharesvsproperty/pages/clone2.html">Still? Try clone 2. Each clone gets 300 uses/ mo</a>
+    <a href="https://github.com/aubcsfw/aussharesvsproperty/pages/index.js">Back to index</a>
       <footer className={styles.footer}>
     <img src="https://raw.githubusercontent.com/aubcsfw/Aussharesvsproperty/refs/heads/main/Coffee.jpg" alt="Thanks" className={styles.logo} />
       </footer>
