1df141b9837c4b725adc06421718704635751738
aubcsfw <aubcsfw@gmail.com>
2025-06-29 20:15:36 +1000

    Create clone2.js

---
+++ pages/clone2.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Shares v property</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+  
+    <iframe src="https://designer.spreadsheetweb.com/a/aus-shares-vs-property-v6" width="460" height="700"></iframe>
+    
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
