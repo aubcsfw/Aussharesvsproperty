5b7fe73cae1fc6d6f5eecff35174fee1b62ab4e4
aubcsfw <aubcsfw@gmail.com>
2025-06-29 20:33:29 +1000

    Update Footer.js
    Revise links

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <>
 
-    <a href="https://aubcsfw.github.io/clone1.js">Not working? Try clone 1. Each clone gets 300 uses/ mo</a>
-    <a href="https://aubcsfw.github.io/clone2.js">Not working? Try clone 2. Each clone gets 300 uses/ mo</a>
+    <a href="https://aubcsfw.github.io/clone1.html">Not working? Try clone 1. Each clone gets 300 uses/ mo</a>
+    <a href="https://github.com/aubcsfw/aussharesvsproperty/pages/clone2.html">Not working? Try clone 2. Each clone gets 300 uses/ mo</a>
       <footer className={styles.footer}>
     <img src="https://raw.githubusercontent.com/aubcsfw/Aussharesvsproperty/refs/heads/main/Coffee.jpg" alt="Thanks" className={styles.logo} />
       </footer>
